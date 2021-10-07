61be0d8fa37e1aa0c1a27c4afaca29fb31eae465
unknown <quentin.floricourt@epitech.eu>
2021-10-08 02:37:38 +0400

    inscription et connexion OK

---
+++ express_order/lib/pages/forgot_pass.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class ForgotPass extends StatefulWidget {
+
+  @override
+  _ForgotPassState createState() => _ForgotPassState();
+}
+
+class _ForgotPassState extends State<ForgotPass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
